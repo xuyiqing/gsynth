5d1261816f5396628eedde9cce91b84acc19ce77
liulch <liulch.16@sem.tsinghua.edu.cn>
2017-09-30 13:52:59 +0800

    Revert "version 1.0.5"
    This reverts commit 9678371c51de3f6e46832f92512816054f53fb8a.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,21 +6,9 @@
 
 using namespace Rcpp;
 
-// data_ub_adj
-arma::mat data_ub_adj(arma::mat I_data, arma::mat data);
-RcppExport SEXP _gsynth_data_ub_adj(SEXP I_dataSEXP, SEXP dataSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::mat >::type I_data(I_dataSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type data(dataSEXP);
-    rcpp_result_gen = Rcpp::wrap(data_ub_adj(I_data, data));
-    return rcpp_result_gen;
-END_RCPP
-}
 // XXinv
 arma::mat XXinv(arma::cube X);
-RcppExport SEXP _gsynth_XXinv(SEXP XSEXP) {
+RcppExport SEXP gsynth_XXinv(SEXP XSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -31,7 +19,7 @@ END_RCPP
 }
 // panel_est
 arma::mat panel_est(arma::cube X, arma::mat Y, arma::mat MF);
-RcppExport SEXP _gsynth_panel_est(SEXP XSEXP, SEXP YSEXP, SEXP MFSEXP) {
+RcppExport SEXP gsynth_panel_est(SEXP XSEXP, SEXP YSEXP, SEXP MFSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -43,22 +31,23 @@ BEGIN_RCPP
 END_RCPP
 }
 // panel_beta
-arma::mat panel_beta(arma::cube X, arma::mat xxinv, arma::mat Y, arma::mat FE);
-RcppExport SEXP _gsynth_panel_beta(SEXP XSEXP, SEXP xxinvSEXP, SEXP YSEXP, SEXP FESEXP) {
+arma::mat panel_beta(arma::cube X, arma::mat xxinv, arma::mat Y, arma::mat F, arma::mat L);
+RcppExport SEXP gsynth_panel_beta(SEXP XSEXP, SEXP xxinvSEXP, SEXP YSEXP, SEXP FSEXP, SEXP LSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< arma::cube >::type X(XSEXP);
     Rcpp::traits::input_parameter< arma::mat >::type xxinv(xxinvSEXP);
     Rcpp::traits::input_parameter< arma::mat >::type Y(YSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type FE(FESEXP);
-    rcpp_result_gen = Rcpp::wrap(panel_beta(X, xxinv, Y, FE));
+    Rcpp::traits::input_parameter< arma::mat >::type F(FSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type L(LSEXP);
+    rcpp_result_gen = Rcpp::wrap(panel_beta(X, xxinv, Y, F, L));
     return rcpp_result_gen;
 END_RCPP
 }
 // panel_factor
 List panel_factor(arma::mat E, int r);
-RcppExport SEXP _gsynth_panel_factor(SEXP ESEXP, SEXP rSEXP) {
+RcppExport SEXP gsynth_panel_factor(SEXP ESEXP, SEXP rSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -68,23 +57,9 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// panel_factor_ub
-List panel_factor_ub(arma::mat E, arma::mat I, int r, double tolerate);
-RcppExport SEXP _gsynth_panel_factor_ub(SEXP ESEXP, SEXP ISEXP, SEXP rSEXP, SEXP tolerateSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::mat >::type E(ESEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type I(ISEXP);
-    Rcpp::traits::input_parameter< int >::type r(rSEXP);
-    Rcpp::traits::input_parameter< double >::type tolerate(tolerateSEXP);
-    rcpp_result_gen = Rcpp::wrap(panel_factor_ub(E, I, r, tolerate));
-    return rcpp_result_gen;
-END_RCPP
-}
 // beta_iter
 List beta_iter(arma::cube X, arma::mat xxinv, arma::mat Y, int r, double tolerate, arma::mat beta0);
-RcppExport SEXP _gsynth_beta_iter(SEXP XSEXP, SEXP xxinvSEXP, SEXP YSEXP, SEXP rSEXP, SEXP tolerateSEXP, SEXP beta0SEXP) {
+RcppExport SEXP gsynth_beta_iter(SEXP XSEXP, SEXP xxinvSEXP, SEXP YSEXP, SEXP rSEXP, SEXP tolerateSEXP, SEXP beta0SEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -98,26 +73,9 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// beta_iter_ub
-List beta_iter_ub(arma::cube X, arma::mat xxinv, arma::mat Y, arma::mat I, int r, double tolerate, arma::mat beta0);
-RcppExport SEXP _gsynth_beta_iter_ub(SEXP XSEXP, SEXP xxinvSEXP, SEXP YSEXP, SEXP ISEXP, SEXP rSEXP, SEXP tolerateSEXP, SEXP beta0SEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::cube >::type X(XSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type xxinv(xxinvSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type Y(YSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type I(ISEXP);
-    Rcpp::traits::input_parameter< int >::type r(rSEXP);
-    Rcpp::traits::input_parameter< double >::type tolerate(tolerateSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type beta0(beta0SEXP);
-    rcpp_result_gen = Rcpp::wrap(beta_iter_ub(X, xxinv, Y, I, r, tolerate, beta0));
-    return rcpp_result_gen;
-END_RCPP
-}
 // inter_fe
 List inter_fe(arma::mat Y, arma::cube X, int r, int force, arma::mat beta0, double tol);
-RcppExport SEXP _gsynth_inter_fe(SEXP YSEXP, SEXP XSEXP, SEXP rSEXP, SEXP forceSEXP, SEXP beta0SEXP, SEXP tolSEXP) {
+RcppExport SEXP gsynth_inter_fe(SEXP YSEXP, SEXP XSEXP, SEXP rSEXP, SEXP forceSEXP, SEXP beta0SEXP, SEXP tolSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -130,40 +88,4 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(inter_fe(Y, X, r, force, beta0, tol));
     return rcpp_result_gen;
 END_RCPP
-}
-// inter_fe_ub
-List inter_fe_ub(arma::mat Y, arma::cube X, arma::mat I, int r, int force, arma::mat beta0, double tol);
-RcppExport SEXP _gsynth_inter_fe_ub(SEXP YSEXP, SEXP XSEXP, SEXP ISEXP, SEXP rSEXP, SEXP forceSEXP, SEXP beta0SEXP, SEXP tolSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::mat >::type Y(YSEXP);
-    Rcpp::traits::input_parameter< arma::cube >::type X(XSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type I(ISEXP);
-    Rcpp::traits::input_parameter< int >::type r(rSEXP);
-    Rcpp::traits::input_parameter< int >::type force(forceSEXP);
-    Rcpp::traits::input_parameter< arma::mat >::type beta0(beta0SEXP);
-    Rcpp::traits::input_parameter< double >::type tol(tolSEXP);
-    rcpp_result_gen = Rcpp::wrap(inter_fe_ub(Y, X, I, r, force, beta0, tol));
-    return rcpp_result_gen;
-END_RCPP
-}
-
-static const R_CallMethodDef CallEntries[] = {
-    {"_gsynth_data_ub_adj", (DL_FUNC) &_gsynth_data_ub_adj, 2},
-    {"_gsynth_XXinv", (DL_FUNC) &_gsynth_XXinv, 1},
-    {"_gsynth_panel_est", (DL_FUNC) &_gsynth_panel_est, 3},
-    {"_gsynth_panel_beta", (DL_FUNC) &_gsynth_panel_beta, 4},
-    {"_gsynth_panel_factor", (DL_FUNC) &_gsynth_panel_factor, 2},
-    {"_gsynth_panel_factor_ub", (DL_FUNC) &_gsynth_panel_factor_ub, 4},
-    {"_gsynth_beta_iter", (DL_FUNC) &_gsynth_beta_iter, 6},
-    {"_gsynth_beta_iter_ub", (DL_FUNC) &_gsynth_beta_iter_ub, 7},
-    {"_gsynth_inter_fe", (DL_FUNC) &_gsynth_inter_fe, 6},
-    {"_gsynth_inter_fe_ub", (DL_FUNC) &_gsynth_inter_fe_ub, 7},
-    {NULL, NULL, 0}
-};
-
-RcppExport void R_init_gsynth(DllInfo *dll) {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
 }
