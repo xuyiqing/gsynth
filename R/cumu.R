13a48c3f02b644fff47dde0c2071eccade7b3dfa
Shiyun(Arthur) Hu <62308700+Shiyun-Hu@users.noreply.github.com>
2025-03-17 10:43:38 +0800

    Update cumu.R

--- R/cumu.R
+++ R/cumu.R
@@ -7,7 +7,7 @@
 #' @param id ID of the units of interest
 #' @return Cumulative effects
 #' @export
-cumuEff <- function(x, cumu=TRUE, period, id=NULL){
+cumuEff <- function(x, cumu=TRUE, period=NULL, id=NULL){
   library(fect)
   out <- fect:::cumuEff(x=x, cumu=cumu, period=period, id=id)
   return(out)
